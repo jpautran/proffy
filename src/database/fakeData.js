b0434adc423b847f2df9f76fb5b45645c2752dc7
Joao Paulo <jpautran1414@gmail.com>
2020-08-12 15:57:19 -0300

    final

---
+++ src/database/fakeData.js
@@ -0,0 +1,35 @@
+const proffys = [
+    {
+        name: "Joao Paulo Autran",
+        avatar:"https://avatars3.githubusercontent.com/u/66528086?s=460&u=423575da73790cb2dd0eb04378754ab211d0a276&v=4",
+        whatsapp: "8383382173",
+        bio: "lorenu udsahd duah udashd uhduashduahdua haudhaus uadhaudduhsa adsadpoqe opoada pok odjaidjqh md,admadjuqdkam dlasdpa",
+        subject: "Artes",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Joao P. Autran",
+        avatar:"https://avatars3.githubusercontent.com/u/66528086?s=460&u=423575da73790cb2dd0eb04378754ab211d0a276&v=4",
+        whatsapp: "8383382173",
+        bio: "lorenu udsahd duah udashd uhduashduahdua haudhaus uadhaudduhsa adsadpoqe opoada pok odjaidjqh md,admadjuqdkam dlasdpa",
+        subject: "Artes",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    },
+    {
+        name: "Joao Paulo",
+        avatar:"https://avatars3.githubusercontent.com/u/66528086?s=460&u=423575da73790cb2dd0eb04378754ab211d0a276&v=4",
+        whatsapp: "8383382173",
+        bio: "lorenu udsahd duah udashd uhduashduahdua haudhaus uadhaudduhsa adsadpoqe opoada pok odjaidjqh md,admadjuqdkam dlasdpa",
+        subject: "Artes",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220]
+    }
+]
--- src/database/test.js
+++ src/database/test.js
@@ -11,7 +11,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Artes",
+        subject: 1,
         cost: "20",
     }
 
@@ -28,5 +28,27 @@ Database.then(async (db) => {
         }
     ]
 
-    await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    //await createProffy(db, {proffyValue, classValue, classScheduleValues})
+
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    const selectClassesAndProffys = await db.all(`
+        SELECT  classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    //console.log(selectClassesAndProffys)
+
+    const selectedClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to < "1300"
+    `)
+
+    console.log(selectedClassesSchedules)
 })
